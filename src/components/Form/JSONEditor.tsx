13fa26dca23a33f68043c4f789ffca46b2948bd3
moodi <moodizone@gmail.com>
2025-01-28 21:52:19 +0330

    🚸 add beautify functionality

--- src/components/Form/JSONEditor.tsx
+++ src/components/Form/JSONEditor.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
-import { TextField } from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
+import CodeIcon from "@mui/icons-material/Code";
+
 import { useFormProvider } from "./FormProvider";
 
 function JSONEditor() {
+  //================================
+  // Init
+  //================================
   const { stringifyForm, setStringifyForm } = useFormProvider();
   const [value, setValue] = React.useState<string>("");
 
+  //================================
+  // Handlers
+  //================================
   // reflect the outer changes to the internal state
   React.useEffect(() => {
     setValue(stringifyForm);
@@ -23,24 +31,51 @@ function JSONEditor() {
     };
   }, [value, setStringifyForm]);
 
+  function handleBeautify() {
+    try {
+      const parsedJson = JSON.parse(value);
+      const beautifiedJson = JSON.stringify(parsedJson, null, 2);
+      setValue(beautifiedJson);
+    } catch {
+      // ignore errors
+    }
+  }
+
+  //================================
+  // Render
+  //================================
   return (
-    <TextField
-      label="JSON Schema"
-      multiline
-      rows={20}
-      fullWidth
-      variant="outlined"
-      value={value}
-      onChange={(event) => {
-        setValue(event.target.value);
-      }}
-      sx={{
-        "& .MuiInputBase-root": {
-          fontFamily: "monospace",
-          fontSize: "14px",
-        },
-      }}
-    />
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <TextField
+          label="JSON Schema"
+          multiline
+          rows={20}
+          fullWidth
+          variant="outlined"
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          sx={{
+            "& .MuiInputBase-root": {
+              fontFamily: "monospace",
+              fontSize: "14px",
+            },
+          }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          variant="outlined"
+          fullWidth
+          startIcon={<CodeIcon />}
+          onClick={handleBeautify}
+        >
+          {"Beautify"}
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
