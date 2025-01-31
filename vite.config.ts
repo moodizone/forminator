db6f019b9f279853da3f983ac15002024f587c75
moodi <moodizone@gmail.com>
2025-01-31 21:03:05 +0330

    🚧 distinguish environments

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/forminator/",
+  base: process.env.NODE_ENV === "development" ? "/" : "/forminator/",
   build: {
     outDir: "dist",
     rollupOptions: {
